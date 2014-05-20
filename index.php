9876c883480a417850f4e96e61b765b686492075
wo@imac <wolfram@wiedner.co.uk>
2014-05-20 18:04:09 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=yes, width=device-width, minimal-ui">
+
+		<!-- 
+
+		<link rel="shortcut icon" type="image/x-icon" href="./static/img/favicon.ico">
+	    <meta name="viewport" content="user-scalable=no, minimum-scale=1, maximum-scale=1, width=device-width">
+
+		
+		<meta name="description" content="Template description."> 
+	    <meta name="author" content="Wolfram Wiedner, wiedner.co.uk">
+	    
+		<meta name="apple-mobile-web-app-capable" content="yes"/>
+		<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+		<link rel="apple-touch-startup-image" href="startup-320x460.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" href="apple-touch-icon-57x57-precomposed.png" />
+		
+		<meta property="og:title" content=""/>
+		<meta property="og:url" content=""/>
+		<meta property="og:image" content=""/>
+		<meta property="og:site_name" content=""/>
+		<meta property="og:description" content=""/>
+		<meta property="og:locale" content="en_gb"/>
+
+		-->
+
+		<link rel="stylesheet" href="./static/css/normalize.css" type="text/css" media="all">
+		<link rel="stylesheet" href="./static/css/template.less?<?php echo time(); ?>" type="text/less" media="all">
+		<script type="text/javascript" src="./static/js/less-1.3.3.min.js"></script>		
+		
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="./static/js/jquery-1.11.0.min.js"></script>
+		<![endif]-->
+		<!--[if gte IE 9]><!-->
+		<script type="text/javascript" src="./static/js/jquery-2.1.1.min.js"></script>
+		<!--<![endif]-->
+
+		<script type="text/javascript" src="./static/js/template.js?<?php echo time(); ?>"></script>
+		
+		<title>Template Title</title>
+		
+	</head>
+
+	<body>
+
+
+		<header>
+			<h1>Page title</h1>
+			<nav>
+			
+			</nav>
+		</header>
+
+		<main>
+		
+		</main>	
+	
+	
+	</body>
+</html>
